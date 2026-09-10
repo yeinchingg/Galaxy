f9fabec521e19da30b1e2ecaa59c3b646817156e
yenching <qwe33370@gmail.com>
2026-09-10 16:49:30 +0800

    push to render

--- main.py
+++ main.py
@@ -86,4 +86,5 @@ if FRONTEND_DIR.exists():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
